9b1d0e8ffe3d38b55a02d5fed0d3d6c2d247687f
Ben Alexander <benjaminalexander747@gmail.com>
2022-10-21 16:58:54 +0100

    printing kinda works now

--- src/ben-matrix/main.c
+++ src/ben-matrix/main.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 int main(void) {
-  matrix *m = zero_matrix(5, 5);
+  matrix *m = zero_matrix(4, 5);
+  print_matrix(m);
 
   printf("program works\n");
 
--- src/ben-matrix/matrix.c
+++ src/ben-matrix/matrix.c
@@ -4,49 +4,41 @@
 
 MATRIX_ERROR M_ERROR = NO_ERROR;
 
-// also unsure
+// issue with memory allocation for more than 20 values
 matrix *zero_matrix(int rows, int cols) {
-    int* body = (int *) malloc(rows * cols * sizeof(int));
-    matrix* zero_matrix = (matrix *) malloc(sizeof(matrix));
+    matrix* m = (matrix *) malloc(sizeof(matrix));
     
-    int body[rows][cols];
+    // set the attributes of m those passed into the fn
+    m->rows = rows;
+    m->columns = cols;
+    // and allocate memory for the array
+    m->body = (int*)malloc(rows * cols * sizeof(int));
+
     for (int i = 0; i < rows*cols; i++) {
-        body[i] = 0;
+        m->body[i] = 0;
     }
-    return NULL; 
+    return m; 
 }
 
 // unsure
 int free_matrix(matrix *m) { 
-    int* arr = m->body;
-    int* r = m->rows;
-    int* c = m->columns;
-    free(c);
-    free(r);
-    free(arr);
     free(m);
     return -1; 
 }
 
-// unsure
+// kinda works
 void print_matrix(matrix *m) {
-    int* r = m->rows;
-    int* c = m->columns;
-    int* arr = m->body;
-
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            //printf("%i", arr[i][j]);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            int index = m->body[i * m->rows + m->columns];
+            printf("%i", index);
         }
         printf("\n");
     }
 }
 
 int get_rows(matrix *m) { 
-
     return m->rows; 
-
-
 }
 
 int get_cols(matrix *m) { 
