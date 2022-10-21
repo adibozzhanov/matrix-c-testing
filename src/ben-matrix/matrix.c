c9ec63cdb80cba96399777d7a3c850bb1db18e8a
Ben Alexander <benjaminalexander747@gmail.com>
2022-10-21 16:10:30 +0100

    trying to make things work but they dont

--- src/ben-matrix/matrix.c
+++ src/ben-matrix/matrix.c
@@ -4,23 +4,54 @@
 
 MATRIX_ERROR M_ERROR = NO_ERROR;
 
+// also unsure
 matrix *zero_matrix(int rows, int cols) {
     int* body = (int *) malloc(rows * cols * sizeof(int));
-    matrix* zero_matrix = (matrix *) malloc(2*sizeof(int));
+    matrix* zero_matrix = (matrix *) malloc(sizeof(matrix));
     
+    int body[rows][cols];
     for (int i = 0; i < rows*cols; i++) {
         body[i] = 0;
     }
     return NULL; 
 }
 
-int free_matrix(matrix *m) { return -1; }
+// unsure
+int free_matrix(matrix *m) { 
+    int* arr = m->body;
+    int* r = m->rows;
+    int* c = m->columns;
+    free(c);
+    free(r);
+    free(arr);
+    free(m);
+    return -1; 
+}
 
-void print_matrix(matrix *m) {}
+// unsure
+void print_matrix(matrix *m) {
+    int* r = m->rows;
+    int* c = m->columns;
+    int* arr = m->body;
 
-int get_rows(matrix *m) { return -1; }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            //printf("%i", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-int get_cols(matrix *m) { return -1; }
+int get_rows(matrix *m) { 
+
+    return m->rows; 
+
+
+}
+
+int get_cols(matrix *m) { 
+    return m->columns; 
+}
 
 int set(matrix *m, int row, int col, int val) { return -1; }
 
